ded3b5adace0b6f5a9c4d74499b135763998d94a
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-06-23 09:30:09 +0530

    Creating web3paypal

---
+++ contracts/Paypal.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Paypal {
+    // Define owner of smart contract
+
+    // Create Struct and Mapping for request, transaction & name
+
+    // Add a name to wallet address
+
+    // Create a request for payment
+
+    // Create a transaction for payment
+
+    // Get all request sent to user
+
+    // Get all transaction sent to user
+
+}
